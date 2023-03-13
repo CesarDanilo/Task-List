492012fd92b7a0b07737d318581cfd57057772fd
Cesar Danilo <billypalacios616@gmail.com>
2023-03-12 21:42:25 -0300

    adicionando nova funcionalidade, mostrar lista de tarefas

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -8,11 +8,37 @@ function CreateLi(label) {
 	return li;
 }
 
+function CriarBtnApagar(li) {
+	li.innerText += ' ';
+	const buttonApagar = document.createElement('button');
+	buttonApagar.setAttribute('class', 'btnApagar');
+	buttonApagar.innerText = 'apagar';
+	li.appendChild(buttonApagar);
+}
+
+lbInput.addEventListener('keypress', function (e) {
+	if (e.keyCode === 13) {
+		if (!lbInput.value) {
+			return;
+		}
+		CreateTask(lbInput.value);
+	}
+});
+
+function LimparInput() {
+	lbInput.value = '';
+	lbInput.focus();
+}
+
 function CreateTask(label) {
 	const li = CreateLi(label);
+	CriarBtnApagar(li);
 	ul.appendChild(li);
+	LimparInput();
 }
 
 btnInput.addEventListener('click', function () {
 	CreateTask(lbInput.value);
 });
+
+document.addEventListener('click', function(event){});
