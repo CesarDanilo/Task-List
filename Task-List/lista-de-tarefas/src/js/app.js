7722519187004da226d0577ed9aec52f8234782d
Cesar Danilo <billypalacios616@gmail.com>
2023-03-31 12:55:40 -0300

    adicionando comentarios para cada função no codigo

--- Task-List/lista-de-tarefas/src/js/app.js
+++ Task-List/lista-de-tarefas/src/js/app.js
@@ -1,33 +1,46 @@
+// Seleciona elementos HTML da página
 const task = document.querySelector('.tarefa');
 const time = document.querySelector('.time');
 const lista = document.querySelector('.lista-de-tarefas');
 const btntarefas = document.querySelector('.btn');
 
-
-
+// Salva as tarefas na localStorage
 function salvartarefas() {
-	let litarefa = lista.querySelectorAll('li');
 	const listastarefas = [];
+	let litarefa = lista.querySelectorAll('li');
 
 	for (let tarefa of litarefa) {
 		listastarefas.push(tarefa.innerText.replace('Deletar', '').trim().toString());
 	}
-	const tarefaJson = JSON.stringify(listastarefas);
-	localStorage.setItem('tasks', tarefaJson);
 
+	const tarefaJson = JSON.stringify(listastarefas);''
+	localStorage.setItem('tasks', tarefaJson);
 }
 
+// Adiciona as tarefas salvas na localStorage
+function adicionatarefas() {
+	const lista_de_itens = localStorage.getItem('tasks');
+	let tarefas = JSON.parse(lista_de_itens);
 
+	for (let i in tarefas) {
+		console.log(tarefas[i]);
+		criarlistatarefas(tarefas[i]);
+	}
+}
+
+// Cria o botão de deletar tarefa
 function criarbtndelete() {
 	const btn = document.createElement('button');
 	return btn;
 }
 
+// Cria o elemento de lista HTML
 function criarLi() {
 	const li = document.createElement('li');
 	return li;
 }
 
+// Cria a lista de tarefas a partir de uma string passada por parâmetro
 function criarlistatarefas(tasklist) {
 	const li = criarLi();
 	const btn = criarbtndelete();
@@ -37,21 +50,25 @@ function criarlistatarefas(tasklist) {
 	li.appendChild(btn);
 	lista.appendChild(li);
 
+	// Salva a lista de tarefas e adiciona um evento de click para deletar tarefas
 	salvartarefas();
 	btn.addEventListener('click', function () {
 		btn.parentElement.remove();
 		salvartarefas();
 	});
 
+	// Limpa os campos de input
 	task.value = '';
 	time.value = '';
 }
 
+// Adiciona evento de click para adicionar tarefas na lista
 btntarefas.addEventListener('click', function () {
 	if (!task.value) return;
 	criarlistatarefas(task.value + ' ' + time.value);
 });
 
+// Adiciona eventos de pressionar tecla Enter para adicionar tarefas na lista
 task.addEventListener('keypress', function (e) {
 	if (e.key == "Enter") {
 		criarlistatarefas(task.value + ' ' + time.value);
@@ -63,15 +80,3 @@ time.addEventListener('keypress', function (e) {
 		criarlistatarefas(task.value + ' ' + time.value);
 	}
 });
-
-function adicionatarefas() {
-	const lista_de_itens = localStorage.getItem('tasks');
-	let tarefas = JSON.parse(lista_de_itens);
-
-	for (let i in tarefas) {
-		console.log(tarefas[i]);
-		criarlistatarefas(tarefas[i]);
-	}
-}
-
-adicionatarefas();
